629203c0c5d63e26f1cc80e6b9650ec5e67d117c
andrc-dev <andrecraveiro95@gmail.com>
2023-11-20 23:14:25 +0000

    feature: make the stage draggable   - added a method to handle the action of dragging the stage on mouse     events

--- src/main/java/com/projects/easyregx/Controller.java
+++ src/main/java/com/projects/easyregx/Controller.java
@@ -2,15 +2,18 @@ package com.projects.easyregx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Controller {
 
+    private double posX = 0;
+    private double posY = 0;
+
     // Scene elements
     @FXML
-    private Label welcomeText;
+    private HBox hBox;
     @FXML
     private Button closeButton;
     @FXML
@@ -33,10 +36,26 @@ public class Controller {
     private Button generateButton;
 
     @FXML
+    public void handleStageDrag() {
+        Stage stage = (Stage) hBox.getScene().getWindow();
+
+        hBox.setOnMousePressed(event -> {
+            posX = stage.getX() - event.getScreenX();
+            posY = stage.getY() - event.getScreenY();
+        });
+
+        hBox.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() + posX);
+            stage.setY(event.getScreenY() + posY);
+        });
+    }
+
+    @FXML
     public void minimize() {
         Stage stage = (Stage) minimizeButton.getScene().getWindow();
         stage.setIconified(true);
     }
+
     @FXML
     public void close() {
         javafx.application.Platform.exit();
--- src/main/java/com/projects/easyregx/Main.java
+++ src/main/java/com/projects/easyregx/Main.java
@@ -2,6 +2,7 @@ package com.projects.easyregx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -9,13 +10,30 @@ import javafx.stage.StageStyle;
 import java.io.IOException;
 
 public class Main extends Application {
+
+    //private double posX = 0;
+    //private double posY = 0;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 250);
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        Scene scene = new Scene(root, 600, 250);
         stage.setTitle("EasyRegx");
-        stage.setScene(scene);
         stage.initStyle(StageStyle.UNDECORATED);
+
+        /* Make Stage Draggable
+        root.setOnMousePressed(event -> {
+            posX = stage.getX() - event.getScreenX();
+            posY = stage.getY() - event.getScreenY();
+        });
+
+        root.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() + posX);
+            stage.setY(event.getScreenY() + posY);
+        });
+         */
+
+        stage.setScene(scene);
         stage.show();
     }
 
