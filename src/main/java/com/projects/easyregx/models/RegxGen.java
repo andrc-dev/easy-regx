f5fd9bfc2494790d2b03efdcd54776f36041a5ff
andrc-dev <andrecraveiro95@gmail.com>
2023-11-21 22:28:10 +0000

    feature: add RegxGen object as the regex generator

---
+++ src/main/java/com/projects/easyregx/models/RegxGen.java
@@ -0,0 +1,21 @@
+package com.projects.easyregx.models;
+
+public class RegxGen {
+
+    private static RegxGen INSTANCE;
+
+    private RegxGen() {
+        System.out.println("RegxGen instantiated.");
+    }
+
+    public static RegxGen getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new RegxGen();
+        }
+        return INSTANCE;
+    }
+
+    public String generate() {
+        return "/regex-text/";
+    }
+}
