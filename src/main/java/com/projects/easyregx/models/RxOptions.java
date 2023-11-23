6b5d6e9ebbdce55f4257a28b9a614b8dbaf33e26
andrc-dev <andrecraveiro95@gmail.com>
2023-11-23 00:54:29 +0000

    code: add forgotten enum values   - added new values: alphanumeric w/ space and combination of all four

--- src/main/java/com/projects/easyregx/models/RxOptions.java
+++ src/main/java/com/projects/easyregx/models/RxOptions.java
@@ -3,10 +3,11 @@ package com.projects.easyregx.models;
 public enum RxOptions {
     NUMBERS("0-9"),
     LETTERS("a-zA-Z"),
-    SYMBOLS(".@,"),
+    SYMBOLS(".@,-"),
     SPACE(" "),
     ALPHANUMERICS("a-zA-Z0-9"),
-    ALPHANUMERICS_AND_SPACE("a-zA-Z0-9 ");
+    ALPHANUMERICS_AND_SPACE("a-zA-Z0-9 "),
+    ALPHANUMERICS_SYMBOLS_SPACE("a-zA-Z0-9.@,- ");
 
     private String string;
     RxOptions(String s) {
